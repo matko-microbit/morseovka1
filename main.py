9ada2d700cc3bf4c613b3db3345e1dfc8b64b597
matko-microbit <85074476+matko-microbit@users.noreply.github.com>
2021-05-30 19:15:04 +0200

    Počiatočné súbory pre projekt MakeCode

---
+++ main.py
@@ -0,0 +1,10 @@
+OLED.init(128, 64)
+_zoznam = [0, 0, 0, 0, 0, 0]
+
+def on_forever():
+    cas_medzery = 0
+    if cas_medzery < 2:
+        pass
+    else:
+        pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,10 @@
+OLED.init(128, 64)
+let _zoznam = [0, 0, 0, 0, 0, 0]
+basic.forever(function () {
+    let cas_medzery = 0
+    if (cas_medzery < 2) {
+    	
+    } else {
+    	
+    }
+})
